cc34ca0c33f92d99baa3f0acddf1dba4fc23f5c5
Jeffrey Liu <jeffliu604@gmail.com>
2024-02-22 14:06:21 -0600

    Link libraries through CMake

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <slang.h>
+#include <vulkan/vulkan.h>
+
 int main() {
     std::cout << "Hello Vulkan!" << std::endl;
 }
