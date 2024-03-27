dc2c1871074c20ed65d4ebdb8b0cdbf04f8871f8
Jeffrey Liu <jeffliu604@gmail.com>
2024-03-26 19:55:38 -0500

    Fix logger compilation

--- Source/CoreUtility/Logging.h
+++ Source/CoreUtility/Logging.h
@@ -3,7 +3,11 @@
 #include <memory>
 #include <string>
 
+#pragma warning( push )
+#pragma warning( disable : 4996 )
+#pragma warning( disable : 6285 )
 #include <spdlog/spdlog.h>
+#pragma warning( pop )
 
 class Logger {
 public:
@@ -19,26 +23,26 @@ public:
     ~Logger() = default;
 
     template<typename... Args>
-    void Log(SeverityLevel level, Args&&... args) {
+    void Log(SeverityLevel level, std::string_view format_string, Args&&... args) {
         switch (level) {
             case SeverityLevel::TRACE: {
-                logger_->trace(std::forward<Args>(args)...);
+                logger_->trace(fmt::runtime(format_string), std::forward<Args>(args)...);
                 break;
             }
             case SeverityLevel::INFO: {
-                logger_->info(std::forward<Args>(args)...);
+                logger_->info(fmt::runtime(format_string), std::forward<Args>(args)...);
                 break;
             }
             case SeverityLevel::WARN: {
-                logger_->warn(std::forward<Args>(args)...);
+                logger_->warn(fmt::runtime(format_string), std::forward<Args>(args)...);
                 break;
             }
             case SeverityLevel::ERROR: {
-                logger_->error(std::forward<Args>(args)...);
+                logger_->error(fmt::runtime(format_string), std::forward<Args>(args)...);
                 break;
             }
             case SeverityLevel::FATAL: {
-                logger_->critical(std::forward<Args>(args)...);
+                logger_->critical(fmt::runtime(format_string), std::forward<Args>(args)...);
                 break;
             }
         }
@@ -49,5 +53,5 @@ private:
 };
 
 #define DEFINE_LOGGER(name, level) Logger logger_##name = Logger(#name, level) 
-#define DEFINE_LOGGER_EXTERN(name, level) extern Logger logger_##name;
-#define LOG(name, level, ...) logger_##name.Log(level, __VA_ARGS__)
+#define DEFINE_LOGGER_EXTERN(name) extern Logger logger_##name;
+#define LOG(name, level, format_string, ...) logger_##name.Log(level, format_string, __VA_ARGS__)
